2703064e419df7162c9492939427de60756beaa8
asper219c22ug219csc025 <144356468+asper219c22ug219csc025@users.noreply.github.com>
2023-09-21 20:23:28 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import math
-
-
-def fact(n):
-  return (math.factorial(n))
-
-
-num = int(input("Enter the number:"))
-f = fact(num)
-print("factorial of", num, "is", num, "is", f)
+def factorial(n):
+    return 1 if (n==1 or n==0)
+  else n * factorial(n-1);
+ 
+#Enter input
+n = int(input("Enter input number : "))
+ 
+print("The factorial of",n,"is",factorial(n))
